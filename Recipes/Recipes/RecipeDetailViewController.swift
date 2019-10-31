7e6e4a2eb42e13c1e787bd95e4798faa17b1be81
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-30 22:11:36 -0400

    Basic configuration of Recipe Detail View Controller

--- Recipes/Recipes/RecipeDetailViewController.swift
+++ Recipes/Recipes/RecipeDetailViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class RecipeDetailViewController: UIViewController {
+    // MARK: - Outlets
     @IBOutlet weak var recipeNameLabel: UILabel!
     @IBOutlet weak var recipeTextView: UITextView!
     
+    // MARK: - Properties
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
+    }
+    
+    func updateViews() {
+        guard let recipe = recipe else { return }
+        if isViewLoaded {
+            recipeNameLabel.text = recipe.name
+            recipeTextView.text = recipe.instructions
+        }
     }
     
 
