92115c21ff2f84765dc0ec34f07fe5f9209d0261
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-30 23:19:00 -0400

    Added title

--- Recipes/Recipes/MainViewController.swift
+++ Recipes/Recipes/MainViewController.swift
@@ -9,25 +9,66 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    @IBOutlet weak var textField: UITextField!
+    // MARK: - Outlets
+    @IBOutlet weak var searchTextField: UITextField!
     
+    // MARK: - Properties
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            self.recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = filteredRecipes
+            recipesTableViewController?.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        networkClient.fetchRecipes { (recipes, error) in
+            if let error = error {
+                NSLog("Error loading data \(error)")
+                return
+            }
+            self.allRecipes = recipes ?? []
+        }
     }
     
+    // MARK: - Actions
     @IBAction func textFieldAction(_ sender: UITextField) {
+        self.searchTextField.resignFirstResponder()
+        filterRecipes()
     }
     
-    /*
+    // MARK: - Methods
+    func filterRecipes() {
+        DispatchQueue.main.async {
+            if let text = self.searchTextField.text, text != "" {
+              self.filteredRecipes = self.allRecipes.filter( {(recipe) in recipe.name.contains(text) || recipe.instructions.contains(text)} )
+            } else {
+            self.filteredRecipes = self.allRecipes
+            }
+        }
+    }
+    
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "embeddedTableViewSegue" {
+            let vc = segue.destination as! RecipesTableViewController
+                recipesTableViewController = vc
+        }
     }
-    */
-
 }
